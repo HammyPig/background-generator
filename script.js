d0040b7ee2f5b73ec20e38e939e3fb7a431d30d6
HammyPig <jameswalden@tutamail.com>
2022-07-06 21:27:24 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function changeColour() {
+	body.style.background = "linear-gradient(to right," + color1.value + ", " + color2.value;
+	css.textContent = body.style.background + ";"
+}
+
+var css = document.querySelector("h3");
+var color1 = document.getElementById("color1");
+var color2 = document.getElementById("color2");
+var body = document.getElementById("gradient");
+
+color1.addEventListener("input", changeColour);
+color2.addEventListener("input", changeColour);
+
+changeColour();
